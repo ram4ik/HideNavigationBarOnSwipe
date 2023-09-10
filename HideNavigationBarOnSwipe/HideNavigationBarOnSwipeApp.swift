2c74491303e0eb4c3cccf81676bf44c0eb2d61e8
Ramill <ramill_ibragimov@icloud.com>
2023-09-10 08:06:17 +0300

    Initial Commit

---
+++ HideNavigationBarOnSwipe/HideNavigationBarOnSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideNavigationBarOnSwipeApp.swift
+//  HideNavigationBarOnSwipe
+//
+//  Created by Ramill Ibragimov on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HideNavigationBarOnSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
